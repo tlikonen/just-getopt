3f64b73da521588ab6d169946deb3bf3efc21fdc
Teemu Likonen <tlikonen@iki.fi>
2025-01-04 17:15:38 +0200

    Improve examples/basic.rs

--- examples/basic.rs
+++ examples/basic.rs
@@ -2,20 +2,21 @@ use just_getopt::{OptFlags, OptSpecs, OptValueType};
 use std::process::ExitCode;
 
 fn main() -> ExitCode {
-    // Create new option specification struct `OptSpecs` with three
-    // different options for logical meanings "help", "file" and
-    // "verbose". All three options can be accessed in command-line with
+    // The `OptSpecs::new()` function below creates a new option
+    // specification struct `OptSpecs`. Its `option()` methods configure
+    // three different options for logical meanings "help", "file" and
+    // "verbose". All three options can be written in command-line with
     // a short variant (like "-h") and a long variant (like "--help").
     // Some options accept or require a value.
     //
     // Flag OptionsEverywhere changes parser's behavior. This flag means
-    // that options and other arguments can be mixed in their order in
-    // the command line. Without the flag the option parsing stops at
-    // the first non-option argument and the rest of the command-line is
-    // parsed as non-options.
+    // that options and other arguments (non-options) can be mixed in
+    // their order in the command line. Without the flag the option
+    // parsing stops at the first non-option argument and the rest of
+    // the command-line is parsed as non-options.
     let specs = OptSpecs::new()
-        .flag(OptFlags::OptionsEverywhere)
-        .option("help", "h", OptValueType::None)
+        .flag(OptFlags::OptionsEverywhere) // Argument: (flag)
+        .option("help", "h", OptValueType::None) // Arguments: (id, name, value_type)
         .option("help", "help", OptValueType::None)
         .option("file", "f", OptValueType::Required)
         .option("file", "file", OptValueType::Required)
@@ -23,10 +24,11 @@ fn main() -> ExitCode {
         .option("verbose", "verbose", OptValueType::Optional);
 
     // Parse program's command-line `std::env::args()` with the given
-    // option specification (`OptSpecs`).
+    // option specification `specs` which is an `OptSpecs` struct.
     let parsed = specs.getopt();
 
-    // With this you can see the parsed Args struct.
+    // With this you can see the parsed output which is an `Args`
+    // struct.
     eprintln!("{:#?}", parsed);
 
     // Report user about unknown options.
@@ -41,7 +43,10 @@ fn main() -> ExitCode {
         return ExitCode::FAILURE;
     }
 
-    // Print help and exit because "-h" or "--help" was given.
+    // Print help and exit because "-h" or "--help" was given. We use
+    // option's identifier string "help" here to find if the correct
+    // option was present in the command line. See the `id` argument of
+    // `option()` methods above.
     match parsed.options_first("help") {
         None => (),
         Some(_) => {
@@ -51,8 +56,8 @@ fn main() -> ExitCode {
     }
 
     // Collect all (required) values for "-f" and "--file". Also collect
-    // all (optional) values for "-v" and "--verbose". These can be
-    // empty vectors if no values were given.
+    // all (optional) values for "-v" and "--verbose". We use options'
+    // identifier (id) strings "file" and "verbose" here.
     for f in &parsed.options_value_all("file") {
         println!("File name: {:?}", f);
     }
@@ -65,10 +70,12 @@ fn main() -> ExitCode {
         println!("Option 'verbose' was given.");
     }
 
-    // Collect all other arguments.
+    // Collect all other (non-option) arguments.
     for o in &parsed.other {
         println!("Other argument: {:?}", o);
     }
 
+    // Try to run this program with various command-line options to see
+    // the output.
     ExitCode::SUCCESS
 }
