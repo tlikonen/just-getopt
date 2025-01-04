5c15f81ef3d67d52ffc96447836444799933bfb7
Teemu Likonen <tlikonen@iki.fi>
2025-01-04 13:37:08 +0200

    Improve the main documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,19 @@
 //! # Introduction
 //!
-//! This crate implements a Posix `getopt`-like command-line option
-//! parser with friendly Rust-like programming interface. It’s *just*
-//! that, hence the name `just_getopt`. The intent is to not add fancy
-//! features or automatic magic.
+//! This library crate implements a Posix `getopt`-like command-line
+//! option parser with simple programming interface. More specifically
+//! the parser is like `getopt`’s GNU extension called `getopt_long`
+//! which is familiar command-line option format for users of
+//! Linux-based operating systems.
 //!
-//! More specifically the parser is like `getopt`’s GNU extension called
-//! `getopt_long` which is familiar format for users of Linux-based
-//! operating systems.
+//! The name is `just_getopt` because this is *just a getopt parser* and
+//! (almost) nothing more. The intent is to provide just the parsed
+//! output and methods for examining the output. There will not be
+//! anything for interpreting the output or for printing messages to
+//! program’s user. The responsibility of interpretation is left to your
+//! program.
 //!
-//! There are two types of command-line options:
+//! In getopt logic there are two types of command-line options:
 //!
 //!  1. short options with a single letter name (`-f`)
 //!  2. long options with more than one letter as their name (`--file`).
