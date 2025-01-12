162c10800d95eccd74b10e1b88b302caaccd4723
Teemu Likonen <tlikonen@iki.fi>
2025-01-12 21:18:00 +0200

    Move test modules to same files as the code

--- src/lib.rs
+++ src/lib.rs
@@ -331,9 +331,6 @@
 
 mod parser;
 
-#[cfg(test)]
-mod tests;
-
 /// Specification for program's valid command-line options.
 ///
 /// An instance of this struct is needed before command-line options can
@@ -825,4 +822,423 @@ pub struct Opt {
     /// that there is no value for the option. Value `Some(String)`
     /// provides a value.
     pub value: Option<String>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn t_create_optspecs() {
+        let mut spec;
+        let mut expect;
+
+        spec = OptSpecs::new().option("help", "help", OptValueType::None);
+        expect = OptSpec {
+            id: String::from("help"),
+            name: String::from("help"),
+            value_type: OptValueType::None,
+        };
+        assert_eq!(1, spec.options.len());
+        assert_eq!(&expect, &spec.options[0]);
+
+        spec = spec.option("file", "f", OptValueType::Optional);
+        expect = OptSpec {
+            id: String::from("file"),
+            name: String::from("f"),
+            value_type: OptValueType::Optional,
+        };
+        assert_eq!(2, spec.options.len());
+        assert_eq!(&expect, &spec.options[1]);
+
+        spec = spec.option("file", "file", OptValueType::Required);
+        expect = OptSpec {
+            id: String::from("file"),
+            name: String::from("file"),
+            value_type: OptValueType::Required,
+        };
+        assert_eq!(3, spec.options.len());
+        assert_eq!(&expect, &spec.options[2]);
+
+        spec = spec.flag(OptFlags::OptionsEverywhere);
+        assert_eq!(true, spec.is_flag(OptFlags::OptionsEverywhere));
+        spec = spec.flag(OptFlags::PrefixMatchLongOptions);
+        assert_eq!(true, spec.is_flag(OptFlags::PrefixMatchLongOptions));
+    }
+
+    #[test]
+    fn t_is_flag() {
+        let mut spec = OptSpecs::new().flag(OptFlags::OptionsEverywhere);
+        assert_eq!(true, spec.is_flag(OptFlags::OptionsEverywhere));
+
+        spec = spec.flag(OptFlags::PrefixMatchLongOptions);
+        assert_eq!(true, spec.is_flag(OptFlags::PrefixMatchLongOptions));
+    }
+
+    #[test]
+    fn t_parsed_output_010() {
+        let parsed = OptSpecs::new()
+            .option("help", "h", OptValueType::None)
+            .option("help", "help", OptValueType::None)
+            .option("file", "f", OptValueType::Required)
+            .option("file", "file", OptValueType::Required)
+            .getopt(["-h", "--help", "-f123", "-f", "456", "foo", "bar"]);
+
+        assert_eq!("h", parsed.options_first("help").unwrap().name);
+        assert_eq!("help", parsed.options_last("help").unwrap().name);
+        assert_eq!("help", parsed.options_first("help").unwrap().id);
+        assert_eq!("help", parsed.options_last("help").unwrap().id);
+        assert_eq!(false, parsed.options_first("help").unwrap().value_required);
+        assert_eq!(false, parsed.options_last("help").unwrap().value_required);
+
+        assert_eq!("f", parsed.options_first("file").unwrap().name);
+        assert_eq!(
+            "123",
+            parsed.options_first("file").unwrap().value.clone().unwrap()
+        );
+        assert_eq!(
+            "456",
+            parsed.options_last("file").unwrap().value.clone().unwrap()
+        );
+        assert_eq!(true, parsed.options_first("file").unwrap().value_required);
+
+        assert_eq!("foo", parsed.other[0]);
+        assert_eq!("bar", parsed.other[1]);
+    }
+
+    #[test]
+    fn t_parsed_output_020() {
+        let parsed = OptSpecs::new()
+            .option("help", "h", OptValueType::None)
+            .getopt(["-h", "foo", "-h"]);
+        assert_eq!("h", parsed.options_first("help").unwrap().name);
+        assert_eq!("foo", parsed.other[0]);
+        assert_eq!("-h", parsed.other[1]);
+    }
+
+    #[test]
+    fn t_parsed_output_030() {
+        let parsed = OptSpecs::new()
+            .flag(OptFlags::OptionsEverywhere)
+            .option("help", "h", OptValueType::None)
+            .option("help", "help", OptValueType::None)
+            .option("file", "f", OptValueType::Required)
+            .option("file", "file", OptValueType::Required)
+            .getopt(["-h", "foo", "--help", "--file=123", "bar", "--file", "456"]);
+
+        assert_eq!("h", parsed.options_first("help").unwrap().name);
+        assert_eq!("help", parsed.options_last("help").unwrap().name);
+        assert_eq!(
+            "123",
+            parsed.options_first("file").unwrap().value.clone().unwrap()
+        );
+        assert_eq!(
+            "456",
+            parsed.options_last("file").unwrap().value.clone().unwrap()
+        );
+        assert_eq!("foo", parsed.other[0]);
+        assert_eq!("bar", parsed.other[1]);
+    }
+
+    #[test]
+    fn t_parsed_output_040() {
+        let parsed = OptSpecs::new()
+            .option("debug", "d", OptValueType::Optional)
+            .option("verbose", "verbose", OptValueType::Optional)
+            .getopt(["-d1", "-d", "--verbose", "--verbose=123"]);
+
+        assert_eq!(
+            "1",
+            parsed
+                .options_first("debug")
+                .unwrap()
+                .value
+                .clone()
+                .unwrap()
+        );
+        assert_eq!(true, parsed.options_last("debug").unwrap().value.is_none());
+        assert_eq!(false, parsed.options_last("debug").unwrap().value_required);
+
+        assert_eq!(
+            true,
+            parsed.options_first("verbose").unwrap().value.is_none()
+        );
+        assert_eq!(
+            "123",
+            parsed
+                .options_last("verbose")
+                .unwrap()
+                .value
+                .clone()
+                .unwrap()
+        );
+        assert_eq!(
+            false,
+            parsed.options_last("verbose").unwrap().value_required
+        );
+    }
+
+    #[test]
+    fn t_parsed_output_050() {
+        let parsed = OptSpecs::new()
+            .option("debug", "d", OptValueType::Optional)
+            .getopt(["-abcd", "-adbc"]);
+
+        assert_eq!(true, parsed.options_first("debug").unwrap().value.is_none());
+        assert_eq!(
+            "bc",
+            parsed.options_last("debug").unwrap().value.clone().unwrap()
+        );
+
+        assert_eq!("a", parsed.unknown[0]);
+        assert_eq!("b", parsed.unknown[1]);
+        assert_eq!("c", parsed.unknown[2]);
+    }
+
+    #[test]
+    fn t_parsed_output_060() {
+        let parsed = OptSpecs::new()
+            .option("aaa", "bbb", OptValueType::None)
+            .option("aaa", "c", OptValueType::None)
+            .option("aaa", "d", OptValueType::None)
+            .option("aaa", "eee", OptValueType::None)
+            .getopt(["--bbb", "-cd", "--eee"]);
+
+        let m = parsed.options_all("aaa");
+        assert_eq!("bbb", m[0].name);
+        assert_eq!("c", m[1].name);
+        assert_eq!("d", m[2].name);
+        assert_eq!("eee", m[3].name);
+    }
+
+    #[test]
+    fn t_parsed_output_070() {
+        let parsed = OptSpecs::new()
+            .flag(OptFlags::PrefixMatchLongOptions)
+            .option("version", "version", OptValueType::None)
+            .option("verbose", "verbose", OptValueType::None)
+            .getopt(["--ver", "--verb", "--versi", "--verbose"]);
+
+        assert_eq!("ver", parsed.unknown[0]);
+        assert_eq!("verb", parsed.options_first("verbose").unwrap().name);
+        assert_eq!("verbose", parsed.options_last("verbose").unwrap().name);
+        assert_eq!("version", parsed.options_first("version").unwrap().id);
+        assert_eq!("versi", parsed.options_first("version").unwrap().name);
+    }
+
+    #[test]
+    fn t_parsed_output_080() {
+        let parsed = OptSpecs::new()
+            // .flag(OptFlags::PrefixMatchLongOptions) Must be commented!
+            .option("version", "version", OptValueType::None)
+            .option("verbose", "verbose", OptValueType::None)
+            .getopt(["--version", "--ver", "--verb", "--versi", "--verbose"]);
+
+        assert_eq!("ver", parsed.unknown[0]);
+        assert_eq!("verb", parsed.unknown[1]);
+        assert_eq!("versi", parsed.unknown[2]);
+        assert_eq!("version", parsed.options_first("version").unwrap().name);
+        assert_eq!("verbose", parsed.options_first("verbose").unwrap().name);
+    }
+
+    #[test]
+    fn t_parsed_output_090() {
+        let parsed = OptSpecs::new()
+            .flag(OptFlags::OptionsEverywhere)
+            .option("help", "h", OptValueType::None)
+            .option("file", "file", OptValueType::Required)
+            .getopt(["-h", "foo", "--file=123", "--", "bar", "--file", "456"]);
+
+        assert_eq!("h", parsed.options_first("help").unwrap().name);
+        assert_eq!("file", parsed.options_first("file").unwrap().name);
+        assert_eq!(
+            "123",
+            parsed.options_first("file").unwrap().value.clone().unwrap()
+        );
+
+        assert_eq!("foo", parsed.other[0]);
+        assert_eq!("bar", parsed.other[1]);
+        assert_eq!("--file", parsed.other[2]);
+        assert_eq!("456", parsed.other[3]);
+    }
+
+    #[test]
+    fn t_parsed_output_100() {
+        let parsed = OptSpecs::new()
+            .option("file", "file", OptValueType::Required)
+            .getopt(["--file=", "--file"]);
+
+        assert_eq!(true, parsed.options_first("file").unwrap().value_required);
+        assert_eq!(
+            "",
+            parsed.options_first("file").unwrap().value.clone().unwrap()
+        );
+        assert_eq!(true, parsed.options_last("file").unwrap().value.is_none());
+    }
+
+    #[test]
+    fn t_parsed_output_110() {
+        let parsed = OptSpecs::new()
+            .option("file", "f", OptValueType::Required)
+            .option("debug", "d", OptValueType::Required)
+            .getopt(["-fx", "-d", "", "-f"]);
+
+        assert_eq!(true, parsed.options_first("file").unwrap().value_required);
+        assert_eq!(
+            "x",
+            parsed.options_first("file").unwrap().value.clone().unwrap()
+        );
+        assert_eq!(true, parsed.options_last("file").unwrap().value.is_none());
+        assert_eq!(
+            "",
+            parsed
+                .options_first("debug")
+                .unwrap()
+                .value
+                .clone()
+                .unwrap()
+        );
+    }
+
+    #[test]
+    fn t_parsed_output_120() {
+        let parsed = OptSpecs::new()
+            .option("file", "f", OptValueType::Required)
+            .option("debug", "d", OptValueType::Required)
+            .getopt(["-f123", "-d", "", "-f", "456", "-f"]);
+
+        let f = parsed.options_value_all("file");
+        let d = parsed.options_value_all("debug");
+
+        assert_eq!(2, f.len());
+        assert_eq!("123", f[0]);
+        assert_eq!("456", f[1]);
+
+        assert_eq!(1, d.len());
+        assert_eq!("", d[0]);
+
+        assert_eq!(true, parsed.options_last("file").unwrap().value.is_none());
+    }
+
+    #[test]
+    fn t_parsed_output_130() {
+        let parsed = OptSpecs::new()
+            .option("file", "file", OptValueType::Required)
+            .option("debug", "debug", OptValueType::Required)
+            .getopt(["--file=123", "--debug", "", "--file", "456", "--file"]);
+
+        let f = parsed.options_value_all("file");
+        let d = parsed.options_value_all("debug");
+
+        assert_eq!(2, f.len());
+        assert_eq!("123", f[0]);
+        assert_eq!("456", f[1]);
+
+        assert_eq!(1, d.len());
+        assert_eq!("", d[0]);
+
+        assert_eq!(true, parsed.options_last("file").unwrap().value.is_none());
+    }
+
+    #[test]
+    fn t_parsed_output_140() {
+        let parsed = OptSpecs::new()
+            .flag(OptFlags::OptionsEverywhere)
+            .option("debug", "d", OptValueType::Optional)
+            .option("debug", "debug", OptValueType::Optional)
+            .getopt([
+                "-d",
+                "-d123",
+                "-d",
+                "--debug",
+                "--debug=",
+                "foo",
+                "--debug=456",
+                "-d",
+            ]);
+
+        let d = parsed.options_all("debug");
+        assert_eq!(7, d.len());
+
+        let d = parsed.options_value_all("debug");
+        assert_eq!(3, d.len());
+        assert_eq!("123", d[0]);
+        assert_eq!("", d[1]);
+        assert_eq!("456", d[2]);
+        assert_eq!("123", parsed.options_value_first("debug").unwrap());
+        assert_eq!("456", parsed.options_value_last("debug").unwrap());
+
+        assert_eq!("foo", parsed.other[0]);
+    }
+
+    #[test]
+    fn t_parsed_output_150() {
+        let parsed = OptSpecs::new().getopt([
+            "-abcd",
+            "-e",
+            "--debug",
+            "--",
+            "--debug=",
+            "foo",
+            "--debug=456",
+        ]);
+
+        assert_eq!(0, parsed.options.len());
+        assert_eq!(3, parsed.other.len());
+        assert_eq!(6, parsed.unknown.len());
+    }
+
+    #[test]
+    fn t_parsed_output_160() {
+        let parsed = OptSpecs::new()
+            .option("file", "file", OptValueType::Required)
+            .getopt(["--file", "--", "--", "--"]);
+
+        assert_eq!(
+            "--",
+            parsed.options_first("file").unwrap().value.clone().unwrap()
+        );
+        assert_eq!(1, parsed.other.len());
+        assert_eq!("--", parsed.other[0]);
+    }
+
+    #[test]
+    fn t_parsed_output_170() {
+        let parsed = OptSpecs::new().getopt(["foo", "bar"]);
+
+        assert_eq!(None, parsed.options_first("not-at-all"));
+        assert_eq!(None, parsed.options_last("not-at-all"));
+    }
+
+    #[test]
+    fn t_parsed_output_180() {
+        let parsed = OptSpecs::new()
+            .option("bar", "bar", OptValueType::None)
+            .getopt(["-aaa", "--foo", "--foo", "--bar=", "--bar="]);
+
+        assert_eq!(3, parsed.unknown.len());
+        assert_eq!("a", parsed.unknown[0]);
+        assert_eq!("foo", parsed.unknown[1]);
+        assert_eq!("bar=", parsed.unknown[2]);
+    }
+
+    #[test]
+    fn t_parsed_output_190() {
+        let parsed = OptSpecs::new()
+            .option("äiti", "äiti", OptValueType::Required)
+            .option("€uro", "€uro", OptValueType::Required)
+            .getopt(["--äiti=ööö", "--€uro", "€€€", "--äiti", "ää", "--äiti"]);
+
+        let a = parsed.options_value_all("äiti");
+        let e = parsed.options_value_all("€uro");
+
+        assert_eq!(2, a.len());
+        assert_eq!("ööö", a[0]);
+        assert_eq!("ää", a[1]);
+
+        assert_eq!(1, e.len());
+        assert_eq!("€€€", e[0]);
+
+        assert_eq!(true, parsed.options_last("äiti").unwrap().value.is_none());
+    }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,5 @@
 use crate::{Args, Opt, OptFlags, OptSpecs, OptValueType};
 
-#[cfg(test)]
-mod tests;
-
 pub fn parse<I>(specs: &OptSpecs, mut iter: I) -> Args
 where
     I: Iterator<Item = String>,
@@ -286,4 +283,274 @@ fn get_short_option_series(s: &str) -> String {
         string.push(*c);
     }
     string
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn t_is_long_option_prefix() {
+        assert_eq!(true, is_long_option_prefix("--ab"));
+        assert_eq!(true, is_long_option_prefix("--abc"));
+        assert_eq!(true, is_long_option_prefix("--a"));
+        assert_eq!(true, is_long_option_prefix("--ä"));
+        assert_eq!(false, is_long_option_prefix("---ab"));
+        assert_eq!(false, is_long_option_prefix("---"));
+        assert_eq!(false, is_long_option_prefix(""));
+        assert_eq!(false, is_long_option_prefix(" "));
+        assert_eq!(false, is_long_option_prefix("-x"));
+        assert_eq!(false, is_long_option_prefix("--"));
+        assert_eq!(false, is_long_option_prefix("-"));
+    }
+
+    #[test]
+    fn t_get_long_option() {
+        assert_eq!("abc", get_long_option("--abc"));
+        assert_eq!("ab", get_long_option("--ab"));
+        assert_eq!("abc=", get_long_option("--abc="));
+        assert_eq!("abc=foo", get_long_option("--abc=foo"));
+        assert_eq!("ä€o=foo", get_long_option("--ä€o=foo"));
+    }
+
+    #[test]
+    #[should_panic]
+    fn t_get_long_option_panic() {
+        get_long_option("-");
+        get_long_option("--");
+        get_long_option("--a");
+    }
+
+    #[test]
+    fn t_get_long_option_name() {
+        assert_eq!("abc", get_long_option_name("--abc"));
+        assert_eq!("ä€", get_long_option_name("--ä€"));
+        assert_eq!("abc", get_long_option_name("--abc="));
+        assert_eq!("abc", get_long_option_name("--abc=1"));
+        assert_eq!("abc", get_long_option_name("--abc=134"));
+        assert_eq!("abc", get_long_option_name("--abc=134="));
+        assert_eq!("abc", get_long_option_name("--abc=134=123"));
+        assert_eq!("abc-def", get_long_option_name("--abc-def=  "));
+        assert_eq!("abc-ä€", get_long_option_name("--abc-ä€=  "));
+    }
+
+    #[test]
+    fn t_is_long_option_equal_sign() {
+        assert_eq!(true, is_long_option_equal_sign("--abc="));
+        assert_eq!(true, is_long_option_equal_sign("--ab="));
+        assert_eq!(true, is_long_option_equal_sign("--ab=1"));
+        assert_eq!(true, is_long_option_equal_sign("--ab=123"));
+        assert_eq!(true, is_long_option_equal_sign("--ä€=123"));
+        assert_eq!(true, is_long_option_equal_sign("--ab=123=123"));
+        assert_eq!(false, is_long_option_equal_sign("--ab"));
+        assert_eq!(false, is_long_option_equal_sign("--a="));
+    }
+
+    #[test]
+    fn t_get_long_option_equal_value() {
+        assert_eq!("", get_long_option_equal_value("--abc"));
+        assert_eq!("", get_long_option_equal_value("--abc="));
+        assert_eq!("1", get_long_option_equal_value("--abc=1"));
+        assert_eq!("=", get_long_option_equal_value("--abc=="));
+        assert_eq!("123", get_long_option_equal_value("--abc=123"));
+        assert_eq!(" 12 3 ", get_long_option_equal_value("--abc= 12 3 "));
+        assert_eq!("123=123=", get_long_option_equal_value("--abc=123=123="));
+        assert_eq!("!", get_long_option_equal_value("--abc-def=!"));
+        assert_eq!("!", get_long_option_equal_value("--abc-ä€=!"));
+        assert_eq!("öOö", get_long_option_equal_value("--abc-ä€=öOö"));
+    }
+
+    #[test]
+    fn t_is_valid_long_option_name() {
+        assert_eq!(true, is_valid_long_option_name("ab"));
+        assert_eq!(true, is_valid_long_option_name("ab-"));
+        assert_eq!(true, is_valid_long_option_name("ab-abc"));
+        assert_eq!(true, is_valid_long_option_name("ä€"));
+        assert_eq!(false, is_valid_long_option_name("a"));
+        assert_eq!(false, is_valid_long_option_name("€"));
+        assert_eq!(false, is_valid_long_option_name("-abc"));
+        assert_eq!(false, is_valid_long_option_name("abc="));
+        assert_eq!(false, is_valid_long_option_name("abc "));
+        assert_eq!(false, is_valid_long_option_name(" abc "));
+        assert_eq!(false, is_valid_long_option_name("abc ab"));
+    }
+
+    #[test]
+    fn t_is_valid_short_option_name() {
+        assert_eq!(true, is_valid_short_option_name("a"));
+        assert_eq!(true, is_valid_short_option_name("ä"));
+        assert_eq!(true, is_valid_short_option_name("€"));
+        assert_eq!(true, is_valid_short_option_name("1"));
+        assert_eq!(true, is_valid_short_option_name("?"));
+        assert_eq!(true, is_valid_short_option_name("="));
+        assert_eq!(true, is_valid_short_option_name("%"));
+        assert_eq!(false, is_valid_short_option_name("-"));
+        assert_eq!(false, is_valid_short_option_name(" "));
+    }
+
+    #[test]
+    fn t_is_short_option_prefix() {
+        assert_eq!(true, is_short_option_prefix("-a"));
+        assert_eq!(true, is_short_option_prefix("-ä"));
+        assert_eq!(true, is_short_option_prefix("-€"));
+        assert_eq!(true, is_short_option_prefix("-="));
+        assert_eq!(true, is_short_option_prefix("-?"));
+        assert_eq!(true, is_short_option_prefix("-abcd"));
+        assert_eq!(false, is_short_option_prefix("-"));
+        assert_eq!(false, is_short_option_prefix("--"));
+        assert_eq!(false, is_short_option_prefix("a"));
+        assert_eq!(false, is_short_option_prefix("aa"));
+        assert_eq!(false, is_short_option_prefix("aaa"));
+        assert_eq!(false, is_short_option_prefix(""));
+        assert_eq!(false, is_short_option_prefix(" "));
+        assert_eq!(false, is_short_option_prefix("- "));
+        assert_eq!(false, is_short_option_prefix("--ab"));
+        assert_eq!(false, is_short_option_prefix("--a"));
+    }
+
+    #[test]
+    fn t_get_short_option_series() {
+        assert_eq!("a", get_short_option_series("-a"));
+        assert_eq!("ab", get_short_option_series("-ab"));
+        assert_eq!("ä€", get_short_option_series("-ä€"));
+        assert_eq!("ab -", get_short_option_series("-ab -"));
+    }
+
+    #[test]
+    fn t_get_short_option_match() {
+        let spec = OptSpecs::new()
+            .option("help", "help", OptValueType::None)
+            .option("verbose", "verbose", OptValueType::None)
+            .option("verbose", "v", OptValueType::None)
+            .option("€uro", "€", OptValueType::None)
+            .option("file", "f", OptValueType::None);
+
+        {
+            let m = &spec.get_short_option_match("v");
+            assert!(m.is_some());
+            let m = m.unwrap();
+            assert_eq!("verbose", m.id);
+            assert_eq!("v", m.name);
+            assert_eq!(OptValueType::None, m.value_type);
+        }
+
+        {
+            let m = &spec.get_short_option_match("f");
+            assert!(m.is_some());
+            let m = m.unwrap();
+            assert_eq!("file", m.id);
+            assert_eq!("f", m.name);
+            assert_eq!(OptValueType::None, m.value_type);
+        }
+
+        {
+            let m = &spec.get_short_option_match("€");
+            assert!(m.is_some());
+            let m = m.unwrap();
+            assert_eq!("€uro", m.id);
+            assert_eq!("€", m.name);
+            assert_eq!(OptValueType::None, m.value_type);
+        }
+
+        {
+            let m = &spec.get_short_option_match("x");
+            assert!(m.is_none());
+        }
+    }
+
+    #[test]
+    fn t_get_long_option_match() {
+        let spec = OptSpecs::new()
+            .option("help", "help", OptValueType::None)
+            .option("verbose", "verbose", OptValueType::None)
+            .option("verbose", "v", OptValueType::None)
+            .option("€uro", "€uro", OptValueType::None)
+            .option("file", "f", OptValueType::None);
+
+        {
+            let m = &spec.get_long_option_match("verbose");
+            assert!(m.is_some());
+            let v = &m.unwrap();
+            assert_eq!("verbose", v.id);
+            assert_eq!("verbose", v.name);
+            assert_eq!(OptValueType::None, v.value_type);
+        }
+
+        {
+            let m = &spec.get_long_option_match("help");
+            assert!(m.is_some());
+            let v = &m.unwrap();
+            assert_eq!("help", v.id);
+            assert_eq!("help", v.name);
+            assert_eq!(OptValueType::None, v.value_type);
+        }
+
+        {
+            let m = &spec.get_long_option_match("€uro");
+            assert!(m.is_some());
+            let v = &m.unwrap();
+            assert_eq!("€uro", v.id);
+            assert_eq!("€uro", v.name);
+            assert_eq!(OptValueType::None, v.value_type);
+        }
+
+        {
+            let m = &spec.get_long_option_match("asdf");
+            assert!(m.is_none());
+        }
+    }
+
+    #[test]
+    fn t_get_long_option_prefix_matches() {
+        let spec = OptSpecs::new()
+            .option("foo", "foo-option", OptValueType::None)
+            .option("bar", "foo-€ö-option", OptValueType::None)
+            .option("verbose", "verbose", OptValueType::None)
+            .option("version", "version", OptValueType::None);
+
+        {
+            let m = &spec.get_long_option_prefix_matches("ver");
+            match m {
+                Some(n) => assert_eq!(2, n.len()),
+                None => panic!("Should not panic!"),
+            };
+        }
+
+        {
+            let m = &spec.get_long_option_prefix_matches("verb");
+            match m {
+                Some(n) => assert_eq!(1, n.len()),
+                None => panic!("Should not panic!"),
+            };
+        }
+
+        {
+            let m = &spec.get_long_option_prefix_matches("foo-");
+            match m {
+                Some(n) => assert_eq!(2, n.len()),
+                None => panic!("Should not panic!"),
+            };
+        }
+
+        {
+            let m = &spec.get_long_option_prefix_matches("foo-€");
+            match m {
+                Some(n) => assert_eq!(1, n.len()),
+                None => panic!("Should not panic!"),
+            };
+        }
+
+        {
+            let m = &spec.get_long_option_prefix_matches("version");
+            match m {
+                Some(n) => assert_eq!(1, n.len()),
+                None => panic!("Should not panic!"),
+            };
+        }
+
+        {
+            let m = &spec.get_long_option_prefix_matches("not-at-all");
+            assert!(m.is_none());
+        }
+    }
 }
